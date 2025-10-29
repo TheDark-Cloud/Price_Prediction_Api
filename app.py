f0337720010b2d8e97d6c36ca2b23312d2689d20
TheDark-Cloud <117770466+TheDark-Cloud@users.noreply.github.com>
2025-10-29 11:40:23 +0000

    using sqlite for prtotyping

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ from dotenv import load_dotenv
 
 def create_app():
     load_dotenv()
-    app = Flask(__name__)
+    my_app = Flask(__name__)
+
+    my_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///features_prediction.db'
+    my_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
